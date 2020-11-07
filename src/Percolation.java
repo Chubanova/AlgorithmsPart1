580ab918d03a03e36101994f583e9ea6e62ac4cc
machuban <machuban@mts.ru>
2020-11-07 08:59:44 +0300

    Percolation

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,18 +1,26 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
-    boolean[][] sites;
-    int size;
-    int opens = 0;
+    private boolean[][] sites;
+    private int rowCount;
+    private int countSites;
+    private int opens;
+    private WeightedQuickUnionUF top;
+    private WeightedQuickUnionUF all;
+    private int sink;
+    private int source;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n > 0) {
-            size = n;
-            sites = new boolean[n][n];
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    sites[i][j] = false;
-                }
-            }
+            rowCount = n;
+            countSites = n * n;
+            sites = new boolean[rowCount][rowCount];
+            all = new WeightedQuickUnionUF(countSites + 2); // includes virtual top bottom
+            top = new WeightedQuickUnionUF(countSites + 1); // includes virtual top
+            opens = 0;
+            sink = countSites + 1;
+            source = countSites;
         } else {
             throw new IllegalArgumentException();
         }
@@ -20,20 +28,61 @@ public class Percolation {
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (row > 0 && col > 0 && row <= size && col <= size) {
+        if (row > 0 && col > 0 && row <= rowCount && col <= rowCount) {
             if (!isOpen(row, col)) {
-                sites[row][col] = true;
+                sites[row - 1][col - 1] = true;
                 opens++;
+                int countOnOrder = encode(row, col);
+                if (row == 1) {  // Top
+                    all.union(source, countOnOrder);
+                    top.union(source, countOnOrder);
+                }
+
+                if (row == rowCount) {  // Bottom
+                    all.union(sink, countOnOrder);
+                }
+
+                // Left
+                if (isActual(row, col - 1) && isOpen(row, col - 1)) {
+                    all.union(countOnOrder, encode(row, col - 1));
+                    top.union(countOnOrder, encode(row, col - 1));
+                }
+
+                //  Right
+                if (isActual(row, col + 1) && isOpen(row, col + 1)) {
+                    all.union(countOnOrder, encode(row, col + 1));
+                    top.union(countOnOrder, encode(row, col + 1));
+                }
+
+                //  Up
+                if (isActual(row - 1, col) && isOpen(row - 1, col)) {
+                    all.union(countOnOrder, encode(row - 1, col));
+                    top.union(countOnOrder, encode(row - 1, col));
+                }
+
+                //Down
+                if (isActual(row + 1, col) && isOpen(row + 1, col)) {
+                    all.union(countOnOrder, encode(row + 1, col));
+                    top.union(countOnOrder, encode(row + 1, col));
+                }
+
+
+            } else {
+                return;
             }
         } else {
             throw new IllegalArgumentException();
         }
     }
 
+    private int encode(int row, int col) {
+        return rowCount * (row - 1) + col-1;
+    }
+
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        if (row > 0 && col > 0 && row <= size && col <= size) {
-            return sites[row][col];
+        if (row > 0 && col > 0 && row <= rowCount && col <= rowCount) {
+            return sites[row - 1][col - 1];
         } else {
             throw new IllegalArgumentException();
         }
@@ -41,11 +90,16 @@ public class Percolation {
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        if (row > 0 && col > 0 && row <= size && col <= size) {
-            return false;
+        if (row > 0 && col > 0 && row <= rowCount && col <= rowCount) {
+            return top.find(source) == top.find(encode(row, col)); // top.connected(source, encode(row,col));
         } else {
             throw new IllegalArgumentException();
         }
+    }
+
+
+    private boolean isActual(int row, int col) {
+        return (row - 1 >= 0 && col - 1 >= 0 && row - 1 < rowCount && col - 1 < rowCount);
     }
 
     // returns the number of open sites
@@ -55,14 +109,8 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        return false;
+        return all.find(source) == all.find(sink);
     }
 
 
-
-    // test client (optional)
-    public static void main(String[] args) {
-
-
-    }
 }
